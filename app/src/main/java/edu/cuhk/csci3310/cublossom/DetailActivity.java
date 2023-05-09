bb48acf3f877e77561cfc53a83b8f0611241c3ca
Xihe4687 <100584264+Xihe4687@users.noreply.github.com>
2023-05-09 20:12:41 +0800

    Update DetailActivity.java
    Testing push

--- app/src/main/java/edu/cuhk/csci3310/cublossom/DetailActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/cublossom/DetailActivity.java
@@ -77,7 +77,7 @@ public class DetailActivity extends AppCompatActivity {
         oFlorence = florence;
         oRichness = richness;
         oFlowerName = flowerName;
-
+        System.out.println("Testing push");
 
         Button buttonFewer = (Button)findViewById(R.id.buttonFewer);
         buttonFewer.setOnClickListener(new View.OnClickListener() {
